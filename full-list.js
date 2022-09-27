11a683b6ceb15211e6176624bf69767bc49881da
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-09-27 11:48:48 +0200

    select function

---
+++ full-list.js
@@ -0,0 +1,26 @@
+const url = "https://www.amiiboapi.com/api/amiibo/";
+
+//console.log(url);
+
+const out = document.querySelector("div#container");
+
+
+function listData(list) {
+  //console.log("List", list);    
+  out.innerHTML = "";
+  let newDivs = "";
+  for (let item of list) {
+      console.log(item);
+        newDivs += `<div>
+        <img src="${item.image}" alt="${item.name}">
+        <h2>${item.name}</h2>
+        <p>From: ${item.amiiboSeries}</p>
+        </div>`;
+  }
+  out.innerHTML = newDivs;
+}
+
+fetch (url)
+.then(data => data.json())
+.then(list => listData(list.amiibo))
+.catch(error => out.innerHTML = "Something's wrong! " + error.message);
--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const url = "https://www.amiiboapi.com/api/";
+const url = "https://www.amiiboapi.com/api/amiibo/?character=";
+
+//console.log(url);
 
 const out = document.querySelector("div#container");
+const select = document.querySelector("select");
 
-function listData(list) {
-    //console.log("List", list);
+let id;
+
+let selectCharacter = () => {
+  let selectedValue = select.value;
+
+  id = selectedValue;
+
+  fetch (url + id)
+  .then(data => data.json())
+  .then(list => listData(list.amiibo))
+  .catch(error => out.innerHTML = "Something's wrong! " + error.message);
+
+  function listData(list) {
+    //console.log("List", list);    
     out.innerHTML = "";
     let newDivs = "";
     for (let item of list) {
@@ -16,3 +31,13 @@ function listData(list) {
     }
     out.innerHTML = newDivs;
 }
+}
+
+select.addEventListener("change", () => {
+  selectCharacter();
+})
+
+
+
+
+  
